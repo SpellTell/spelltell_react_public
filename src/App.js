3b21a5965cd1379db4aafd20d20e50d0f58c09db
Marko R <info@xdmr.us>
2022-10-19 00:04:40 -0400

    form init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import { useState, useReducer } from 'react';
 import './App.css';
 
+
+
 function App() {
+  const [wordCurent, setWordCurrent] = useState();
+
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    setWordCurrent("");
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="SpellTellInput"  autoComplete="off"
+          value={wordCurent} onChange={(e)=>(setWordCurrent(e.target.value))} />
+        </form>
+        <h2>{wordCurent}</h2>
     </div>
   );
 }

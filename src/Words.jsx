83755bd85f60c5443db260f711abc8f88bcf1cb1
Marko R <info@xdmr.us>
2022-10-22 23:50:15 -0400

    add favicon

--- src/Words.jsx
+++ src/Words.jsx
@@ -1,7 +1,8 @@
 const Data = [
-    { id: 1, word: "one", pronunciation: "home", progress: [0,0,0] },
-    { id: 2, word: "two", pronunciation: "trying", progress: [0,0,0] },
-    { id: 3, word: "three", pronunciation: "happy", progress: [0,0,0] },
+    { id: 1, word: "first", pronunciation: "first", progress: [0,0,0] },
+    { id: 2, word: "second", pronunciation: "second", progress: [0,0,0] },
+    { id: 3, word: "third", pronunciation: "third", progress: [0,0,0] },
+    { id: 4, word: "forth", pronunciation: "forth", progress: [0,0,0] },
 
   ];
   export default Data;

0c3dd204f3c46ef1888056496d5f0d3efbdbe19c
Marko R <info@xdmr.us>
2022-10-25 00:24:41 -0400

    Fixed flag; Upated home: pre-box section

--- src/Home.jsx
+++ src/Home.jsx
@@ -9,7 +9,22 @@ import SpellTell from "./Box/SpellTell";
 function Home() {
     return (
         <div className="main">
+
+            <div className="container text-center ">
+                <h1>Improve Your <span>Spelling</span></h1>
+                <div className="subheadline">
+                    <h2>Thousands of adults have improved their spelling
+                        in just <strong style={{color:"rgb(111, 0, 255)"}}> 7 days.</strong> <br />
+                        <strong style={{color:"rgb(0, 128, 92)"}}> No registration</strong> needed,
+                            just start typing to start practicing!<br></br>
+                    </h2>
+                </div>
+            </div>
+
             <SpellTell />
+
+
+
         </div>
     )
 }

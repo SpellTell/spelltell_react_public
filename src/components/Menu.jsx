e68bfee741c6ffa36243ce221375f37d18a486d6
Marko R <info@xdmr.us>
2022-10-24 20:11:22 -0400

    menu: adding function

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,24 +1,48 @@
+import { useEffect, useState } from 'react';
 import React from 'react'
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import logo from '../img/logos/logo-spelltell.png'
 
 
 function Menu() {
+    const [padding, setPadding]=useState(window.innerWidth*0.5);
+
+    useEffect(() => {
+        function handleResize() {
+            setPadding( window.innerWidth*0.5)
+        }
+        window.addEventListener('resize', handleResize)
+    })
+
+
+    function openSlideMenu() {
+        // const padding = (window.innerWidth * 0.5);
+        document.getElementById('slide-menu').style.width = padding + "px";
+        document.getElementById('slide-menu').style.paddingLeft = `${padding - 200}` + "px";
+        //document.getElementById('spelltellBox').style.left = '150px';
+    }
+
+    function closeSlideMenu() {
+        document.getElementById('slide-menu').style.width = '0';
+        document.getElementById('slide-menu').style.paddingLeft = '0px';
+        //  document.getElementById('spelltellBox').style.left = '0px';
+    }
+
     return (
         <>
             <div className="navi-logo">
-                <Link to="index.html"><img className="img-resp" src={logo}/></Link>
+                <Link to="index.html"><img className="img-resp" src={logo} /></Link>
             </div>
 
             <div className="navi-container">
                 <div className="container">
                     <nav className="navi">
                         <span className="open-slide">
-                            <Link to="#" onclick="openSlideMenu()">
+                            <Link to="#" onClick={() => { openSlideMenu() }}>
                                 <svg width="30" height="20">
-                                    <path d="M0,2 30, 2" stroke="rgb(181, 1, 157)" stroke-width="2.5"></path>
-                                    <path d="M0,10 30, 10" stroke="rgb(181, 1, 157)" stroke-width="2.5"></path>
-                                    <path d="M0, 17.5 30, 17.5" stroke="rgb(181, 1, 157)" stroke-width="2.5"></path>
+                                    <path d="M0,2 30, 2" stroke="rgb(181, 1, 157)" strokeWidth="2.5"></path>
+                                    <path d="M0,10 30, 10" stroke="rgb(181, 1, 157)" strokeWidth="2.5"></path>
+                                    <path d="M0, 17.5 30, 17.5" stroke="rgb(181, 1, 157)" strokeWidth="2.5"></path>
                                 </svg>
                             </Link>
                         </span>
@@ -30,17 +54,17 @@ function Menu() {
                 </div>
             </div>
             <div id="slide-menu" className="slide-nav">
-                <Link id="close-button" to="#" className="btn-close" onclick="closeSlideMenu()">
+                <Link id="close-button" to="#" className="btn-close" onClick={() => { closeSlideMenu() }}>
 
                     <svg width="20" height="20">
-                        <path d="M0,0 20, 20" stroke="white" stroke-width="2"></path>
-                        <path d="M0,20 20,  0" stroke="white" stroke-width="2"></path>
+                        <path d="M0,0 20, 20" stroke="white" strokeWidth="2"></path>
+                        <path d="M0,20 20,  0" stroke="white" strokeWidth="2"></path>
                     </svg>
                 </Link>
-                <a to="#">Contact</a>
-                <a to="#">Sing In</a>
-                <a to="#">Sign Up</a>
-                <a to="#">About Us</a>
+                <Link to="#">Contact</Link>
+                <Link to="#">Sing In</Link>
+                <Link to="#">Sign Up</Link>
+                <Link to="#">About Us</Link>
             </div>
         </>
     )

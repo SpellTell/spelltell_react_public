11a361826c840de380f976be192f0fabf24888c7
Marko R <info@xdmr.us>
2022-10-27 01:06:51 -0400

    Subscribe page added

--- src/SignUp.jsx
+++ src/SignUp.jsx
@@ -6,33 +6,16 @@ import { Link } from 'react-router-dom';
 
 function SignUp() {
     const mc = useContext(MenuContext);
+    const emailForm = '<iframe width="540" height="850" src="https://d48d886b.sibforms.com/serve/MUIEAFpBY2HJzLc37JF2DH9jeB3EmPAIbUCAFq1VhRTjwM8L6sSw_vsEpno17kLBN4Jv2jf9SgdOwvhFMPGVSBskTe3Bt2N2S0UfC9Js9WHFb78qowVG26WXEscXkavOKpeSXmYnHq1JgvJ7ICKi-OaVmC3ZLYzbFeKdLo5oy0GWSy57KZa8wOJ9ej6QNuyr9c-c2RmQ4afaY3ta" frameborder="0" scrolling="auto" allowfullscreen style="display: block;margin-left: auto;margin-right: auto;max-width: 100%;"></iframe>';
     return (
         <div className='main' onClick={() => { mc.setIsOpen(false) }}>
             <div className="container split">
                 <div className="page column">
-                    <h2>Be first to use full version of SpellTell</h2>
                     <div className="cta-box">
                         <h2>"Smart way to grow"</h2>
                     </div>
-
-                    <p><img className="img-fluid logo" src={logo} alt="" /> is an educational app that is still in development. If you want to add your email to the waiting list for SpellTell, please write us at hello@<img className="img-fluid logo" src={logo} alt="" />.com.
-                    </p>
-                    <p><img className="img-fluid logo" src={logo} alt="" /> Team, <br />
-                        Happy spelling!</p>
-
+                    {<div dangerouslySetInnerHTML={{ __html: emailForm }} />}
                 </div>
-            </div>
-
-            <div className="container text-center start-learning" style={{ paddingBottom: "120px" }}>
-                <h3 className="learn-with">Start learning with <span><img
-                    className="img-fluid" alt="learning to spell with SpellTell"
-                    src={logo} /></span>
-                    for free. </h3>
-                <Link to="/#spelltellBox">
-                    <div className="learn-with-button">
-                        Start Learning
-                    </div>
-                </Link>
             </div>
 
         </div>
